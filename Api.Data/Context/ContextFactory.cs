70271c22c6cf39aacb6fbf4e91e73e076a50dddc
EricaReis <erica@cloudmed.io>
2021-08-18 11:59:04 -0300

    add BaseRepository and netcore3.1 version

--- Api.Data/Context/ContextFactory.cs
+++ Api.Data/Context/ContextFactory.cs
@@ -8,7 +8,7 @@ namespace Api.Data.Context
         public MyContext CreateDbContext(string[] args)
         {
             //Usado para Criar as Migrações
-            var connectionString = "Server=localhost;Port=3306;DataBase=dbAPI;Uid=root;Pwd=SuaSenha";
+            var connectionString = "Server=localhost;Port=3306;DataBase=dbAPI;Uid=root;Pwd=mudar@123";
             var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
             optionsBuilder.UseMySql(connectionString);
             return new MyContext(optionsBuilder.Options);
