9dcf271f4529087d1a5f3f04014329832a26b6b6
EricaReis <erica@cloudmed.io>
2021-08-20 11:52:03 -0300

    add configureDependencies

---
+++ Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -0,0 +1,14 @@
+using Api.Data.Repository;
+using Api.Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.CrossCutting.DependencyInjection
+{
+    public class ConfigureRepository
+    {
+        public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+        }
+    }
+}
